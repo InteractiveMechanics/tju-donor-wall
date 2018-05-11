fea340af35b29ad5a9343dfda311852a943687ee
Michael Tedeschi <mike.tedeschi@gmail.com>
2018-05-11 13:54:34 -0400

    Removed cached varaibles.js file

--- variables.js
+++
@@ -1,3 +0,0 @@
- var plainData = "http://dev.interactivemechanics.com/tju-donor-wall-cms/index.php/wp-json/wp/v2/posts/";
- var givingData = 'http://dev.interactivemechanics.com/tju-donor-wall-cms/index.php/wp-json/rest-routes/v2/giving';
- var mediaPath = "http://dev.interactivemechanics.com/tju-donor-wall-cms/index.php/wp-json/wp/v2/media/";
